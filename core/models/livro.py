046a3c15c6882cc2c6b066067a545ea9e36cd2c5
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-04-22 08:41:04 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -20,4 +20,4 @@ class Livro(models.Model):
         Autor, related_name='livros', blank=True
     )
     def __str__(self):
-        return f"({self.id}) {self.titulo} ({self.quantidade})"
+        return f"({self.id}) {self.titulo} ({self.quantidade})" 
