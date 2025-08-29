7f9b6230b85adf8c37290027ac1870624236515b
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-08-29 14:31:53 -0300

    feat: aula 31

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -13,7 +13,26 @@ class ItensCompraSerializer(ModelSerializer):
         fields = ('livro', 'quantidade', 'total')
         depth = 1
 
-        
+
+from rest_framework.serializers import (
+    CharField,
+    CurrentUserDefault,
+    HiddenField,
+    ModelSerializer,
+    SerializerMethodField,
+    ValidationError, # novo
+)
+
+class ItensCompraCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ('livro', 'quantidade')
+
+    def validate(self, item):
+        if item['quantidade'] > item['livro'].quantidade:
+            raise ValidationError('Quantidade de itens maior do que a quantidade em estoque.')
+        return item
+
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.e-mail', read_only=True)
